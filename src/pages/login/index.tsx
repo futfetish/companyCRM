bdbae8a7c688eeb63dab53034ebbd3c3b937e833
futfetish <gr1zzly.medvegonok@gmail.com>
2024-09-30 14:52:28 +0300

    login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,19 @@
+import { useSession } from "next-auth/react";
+import { LoginCard } from "~/shared/auth/loginCard";
+import { cn } from "~/shared/utils";
+
+export default function Login() {
+  return (
+    <div
+      className={cn(
+        "w-[100svw]",
+        "h-[100svh]",
+        "flex",
+        "items-center",
+        "justify-center",
+      )}
+    >
+      <LoginCard />
+    </div>
+  );
+}
