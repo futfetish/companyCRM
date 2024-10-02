dce95253aa7ce519bb5d6daae5498b80e225a946
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-02 12:54:16 +0300

    default avarar

---
+++ prisma/migrations/20241002095345_default_avatar/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `company` MODIFY `image` VARCHAR(191) NOT NULL DEFAULT 'https://storage.jobmarket.com.cy/static/default-company-avatar.jpg';
+
+-- AlterTable
+ALTER TABLE `employee` MODIFY `image` VARCHAR(191) NOT NULL DEFAULT 'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg';
