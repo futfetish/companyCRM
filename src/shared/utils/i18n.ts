408948e94c3248f3fc95664a0590b1ad9ae17e8c
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-05 13:40:15 +0300

    i18n util fix

--- src/shared/utils/i18n.ts
+++ src/shared/utils/i18n.ts
@@ -1,8 +1,9 @@
 export const i18n = (i18n : Record<string , Record<string , string>> ,key: string, locale: string) => {
     // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-    if (i18n[locale] && i18n[locale][key]) {
-        return i18n[locale][key]; 
+    if (i18n[key] && i18n[key][locale]) {
+        return i18n[key][locale]; 
     }
+
     return key; 
   };
   
