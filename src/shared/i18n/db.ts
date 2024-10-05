13bf58053da62c5aeb1af26a13edf937e2054a53
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-05 13:36:15 +0300

    EmploymentTypeI18n

---
+++ src/shared/i18n/db.ts
@@ -0,0 +1,19 @@
+import { EmploymentType } from "@prisma/client";
+import { i18n } from "../utils/i18n";
+
+type EmploymentTypeI18n =  Record< EmploymentType , {ru : string} >
+
+const employmentTypeI18n : EmploymentTypeI18n = {
+    staff: { ru: "Сотрудник" },
+    piecework: { ru: "Сдельный" },
+    candidate: { ru: "Кандидат" },
+    archive: { ru: "Архив" },
+    partner: { ru: "Партнер" },
+    other: { ru: "Прочее" },
+
+}
+
+export const getEmploymentType = ( employmentTypeKey : EmploymentType  , locale : keyof EmploymentTypeI18n[EmploymentType] ) => {
+    return i18n( employmentTypeI18n , employmentTypeKey , locale  )
+}
+
