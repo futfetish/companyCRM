f62a81c8ae0c823a69e0050c163e5541b9fe1a7d
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-07 14:54:26 +0300

    employee api

---
+++ src/server/api/routers/employee.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import {
+  EmploymentStatus,
+  EmploymentType,
+  MaritalStatus,
+} from "@prisma/client";
+
+export const employeeRouter = createTRPCRouter({
+  select: publicProcedure
+    .input(
+      z.object({
+        companyIds: z.array(z.number()).optional(), // Массив companyId
+        positionIds: z.array(z.number()).optional(), // Массив positionId
+        types: z.array(z.nativeEnum(EmploymentType)).optional(), // Массив employment type
+        isFavorite: z.boolean().optional(), // Фильтр по isFavorite
+        status: z.array(z.nativeEnum(EmploymentStatus)).optional(), // Массив статусу
+        maritalStatuses: z.array(z.nativeEnum(MaritalStatus)).optional(), // Массив maritalStatus
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const {
+        companyIds,
+        positionIds,
+        types,
+        isFavorite,
+        status,
+        maritalStatuses,
+      } = input;
+
+      const employees = await ctx.db.employee.findMany({
+        where: {
+          ...(companyIds && { companyId: { in: companyIds } }), // Фильтр по companyId
+          ...(positionIds && { positionId: { in: positionIds } }), // Фильтр по positionId
+          ...(types && { type: { in: types } }), // Фильтр по типам занятости
+          ...(isFavorite !== undefined && { isFavorite }), // Фильтр по isFavorite
+          ...(status && { status: { in: status } }), // Фильтр по статусу
+          ...(maritalStatuses && { maritalStatus: { in: maritalStatuses } }), // Фильтр по maritalStatus
+        },
+        include: {
+          company: true, // Подключаем информацию о компании
+          position: true, // Подключаем информацию о должности
+        },
+      });
+
+      return employees;
+    }),
+});
