ecf5a27c1313a2cd77dd90ef919a1d92cf834e25
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-11 13:42:53 +0300

    tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
   theme: {
   	extend: {
   		fontFamily: {
-  			sans: [ "Nunito Sans", 'sans-serif'] 
+  			sans: [ "Nunito Sans", 'sans-serif']
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
@@ -55,6 +55,28 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
