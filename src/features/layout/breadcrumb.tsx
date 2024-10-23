689f0b7f05bbbb3fd5ad4597af7ea8a8c6e3c798
futfetish <gr1zzly.medvegonok@gmail.com>
2024-10-23 14:53:16 +0300

    breadcrump params validation

--- src/features/layout/breadcrumb.tsx
+++ src/features/layout/breadcrumb.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import React, { FC, ReactNode } from "react";
-import { getRoute } from "~/shared/i18n/routes";
+import { getRoute as getRouteI18n } from "~/shared/i18n/routes";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -14,11 +14,20 @@ export const BreadCrumbLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const router = useRouter();
   const path = router.pathname.split("/");
   console.log(path.slice(0, 1).join("/"));
+
+  const getRoute = (route: string): string => {
+    if (route.startsWith("[") && route.endsWith("]")) {
+      return (router.query[route.slice(1, -1)] ?? route) as string;
+    } else {
+      return getRouteI18n(route, "ru");
+    }
+  };
+
   return (
     <div className="flex h-full w-full flex-col bg-[#F9FAFC]">
       <div className="flex h-[80px] flex-shrink-0 items-center p-[24px] text-[30px]">
         {path.length == 2 ? (
-          <strong> {getRoute(path[1]!, "ru").toUpperCase()}</strong>
+          <strong> {getRoute(path[1]!).toUpperCase()}</strong>
         ) : (
           <Breadcrumb>
             <BreadcrumbList className="text-[30px]">
@@ -26,11 +35,13 @@ export const BreadCrumbLayout: FC<{ children: ReactNode }> = ({ children }) => {
                 <React.Fragment key={index}>
                   <BreadcrumbItem>
                     {index < path.length - 1 ? (
-                      <BreadcrumbLink href={path.slice(0, index + 1).join("/") || '/'}>
-                        {getRoute(route, "ru")}{" "}
+                      <BreadcrumbLink
+                        href={path.slice(0, index + 1).join("/") || "/"}
+                      >
+                        {getRoute(route)}
                       </BreadcrumbLink>
                     ) : (
-                      <BreadcrumbPage> {getRoute(route, "ru")} </BreadcrumbPage>
+                      <BreadcrumbPage> {getRoute(route)} </BreadcrumbPage>
                     )}
                   </BreadcrumbItem>
                   {index < path.length - 1 && (
